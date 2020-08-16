d3a3afa275b646e51a3ee4f838a1c8e40d45ba70
Sushant Nayak <sushantnayak@Sushants-iMac-Pro.local>
2020-08-16 18:50:13 -0400

    Initial Commit

---
+++ First.py
@@ -0,0 +1,19 @@
+import math
+import os
+import sys
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+# print(greet('World'))
+print(greet("Corey"))
+r = requests.get("http://lambdaonelimited.com")
+print(r.status_code)
